20e2510afb15fe691e16f135283e627fa9b7c789
Anwesh43 <anweshthecool0@gmail.com>
2019-12-16 05:29:57 +0530

    adding BallUpBouncyView class

--- ballupbouncyview/src/main/java/com/anwesh/uiprojects/ballupbouncyview/BallUpBouncyView.kt
+++ ballupbouncyview/src/main/java/com/anwesh/uiprojects/ballupbouncyview/BallUpBouncyView.kt
@@ -56,3 +56,21 @@ fun Canvas.drawBUBNode(i : Int, scale : Float, curr : Boolean, paint : Paint) {
     }
     restore()
 }
+
+class BallUpBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
