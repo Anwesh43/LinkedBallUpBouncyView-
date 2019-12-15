c5f4036eb62fa743b3696d3b053a621adc5d3a1d
Anwesh43 <anweshthecool0@gmail.com>
2019-12-16 05:13:37 +0530

    adding constants and libraries

--- ballupbouncyview/src/main/java/com/anwesh/uiprojects/ballupbouncyview/BallUpBouncyView.kt
+++ ballupbouncyview/src/main/java/com/anwesh/uiprojects/ballupbouncyview/BallUpBouncyView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.ballupbouncyview
 /**
  * Created by anweshmishra on 16/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 2.9f
