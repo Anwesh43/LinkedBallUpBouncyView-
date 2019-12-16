8e0c8e2b4fc1ad18dd200cf7c69206f84bebd80a
Anwesh43 <anweshthecool0@gmail.com>
2019-12-16 05:42:59 +0530

    adding BallUpBouncy class

--- ballupbouncyview/src/main/java/com/anwesh/uiprojects/ballupbouncyview/BallUpBouncyView.kt
+++ ballupbouncyview/src/main/java/com/anwesh/uiprojects/ballupbouncyview/BallUpBouncyView.kt
@@ -163,4 +163,27 @@ class BallUpBouncyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BallUpBouncy(var i : Int) {
+
+        private var curr : BUBNode = BUBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, curr.i, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
